d3de712c5133a14eb8bb154fe171af09162a92d5
tonmoy054 <alabuA123>
2021-08-21 17:27:39 +0600

    responsivenss 2

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {git push <remote> <branch>
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
