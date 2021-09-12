1c22a7432c6300bbc22aac1c475f7acef782421c
WallE1602 <sefatulwasi@gmail.com>
2021-09-12 17:02:35 +0600

    Some API problems fixed-others in progress

--- API/platoapi/Student/dbconnect.php
+++ API/platoapi/Student/dbconnect.php
@@ -6,4 +6,10 @@ $password = "";
 
 $conn = new Mysqli($servername, $username, $password, $dbname);
 
+// if ($conn -> connect_error) {
+// 	die("error".$conn->connect_error);
+// }
+// echo "success";
+
+
  ?>
