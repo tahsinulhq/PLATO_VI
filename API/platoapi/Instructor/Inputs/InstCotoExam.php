5ae6ea8255b0621571c86bddafe72aa38c7d63a5
Kazi Tahsinul Haque <tahsinul.hq4@gmail.com>
2021-09-13 09:40:45 +0600

    CO to Exam API slightly updated

--- API/platoapi/Instructor/Inputs/InstCotoExam.php
+++ API/platoapi/Instructor/Inputs/InstCotoExam.php
@@ -44,19 +44,11 @@ include "dbconnect.php";
 	VALUES('$sec', '$co', '$qno', '$assessname', '$totmark')
     ";
 
-    $result = mysqli_query($conn, $sql);
-
-    if (mysqli_num_rows($result) > 0) {
-        // output data of each row
-      $rows = array();
-       while($r = mysqli_fetch_assoc($result)) {
-          // $rows["result"][] = $r; // with result object
-         $rows[] = $r; // only array
-       }
-      echo json_encode($rows);
-
+    
+    if (mysqli_query($conn, $sql)) {
+        echo '{"result": "Success"}';
     } else {
-        echo '{"result": "No data found"}';
+        echo '{"result": "Sql error"}';
     }
 }
 
